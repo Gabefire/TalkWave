f9ee35ffd971cee656097892e8041fd2df7cfe19
Gabefire <33893811+Gabefire@users.noreply.github.com>
2024-04-08 20:48:23 -0500

    setup context for channel list

--- src/components/main/main_root.tsx
+++ src/components/main/main_root.tsx
@@ -1,21 +1,34 @@
 import { Outlet, useNavigate } from "react-router-dom";
 import "./main.css";
-
 import Header from "./header/header";
 import SideBar from "./side_bar/side_bar";
 import axios from "axios";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useReducer, useState } from "react";
 import { AuthContext } from "../../contexts/authProvider";
+import { ACTION_TYPE, channelListReducer } from "../../reducers/channelReducer";
+import { channelType } from "../../types/messages";
+import { createContext } from "vm";
+
+const initialState = [] as channelType[];
+type channelListContextType = {
+  channelDispatch: channelType[];
+  dispatch: React.Dispatch<ACTION_TYPE>;
+};
+
+const ChannelListContext = createContext({} as channelListContextType);
 
 function MainRoot() {
   const navigate = useNavigate();
   const [valid, setValid] = useState(false);
   const { token } = useContext(AuthContext);
+  const [channelDispatch, dispatch] = useReducer(
+    channelListReducer,
+    initialState
+  );
 
   useEffect(() => {
     if (!localStorage.getItem("auth")) {
       setValid(false);
-      console.error("No token present");
       navigate("/");
     } else {
       axios.defaults.headers.common["Authorization"] =
@@ -25,7 +38,7 @@ function MainRoot() {
   }, [navigate, token]);
 
   return (
-    <>
+    <ChannelListContext.Provider value={{ channelDispatch, dispatch }}>
       {valid ? (
         <div className="app">
           <Header />
@@ -35,7 +48,7 @@ function MainRoot() {
           <SideBar />
         </div>
       ) : undefined}
-    </>
+    </ChannelListContext.Provider>
   );
 }
 
