cd6bbbeec702bd168728fccbf1f734326e94eacf
Gabefire <33893811+Gabefire@users.noreply.github.com>
2024-06-18 12:36:58 -0500

    switched to longpolling

--- src/components/main/message/messages.tsx
+++ src/components/main/message/messages.tsx
@@ -23,7 +23,8 @@ function Messages() {
     const connection = new signalR.HubConnectionBuilder()
       .withUrl(`${import.meta.env.VITE_WEB_SOCKET_URL}/api/Message`, {
         accessTokenFactory: () => user.token as string,
-        transport: signalR.HttpTransportType.LongPolling,
+        skipNegotiation: true,
+        transport: signalR.HttpTransportType.WebSockets,
       })
       .configureLogging(signalR.LogLevel.Debug)
       .withAutomaticReconnect()
